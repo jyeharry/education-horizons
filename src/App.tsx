d7a27c6d6409152074b819aeca5e221b1f935e9b
Jye Harry <jyeharry21@hotmail.com>
2022-12-02 19:18:43 +1100

    First attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface RGB {
+  red: number
+  green: number
+  blue: number
 }
 
-export default App;
+interface ColourBoxProps extends RGB {}
+
+interface Cache {
+  [k: string]: { red: number; green: number; blue: number }
+}
+
+const ColourBox = styled.div.attrs<ColourBoxProps>(({red, green, blue}) => ({
+  style: {
+    backgroundColor: `rgb(${red}, ${green}, ${blue})`
+  }
+}))<ColourBoxProps>`
+  height: 30px;
+  width: 30px;
+  display: inline-block;
+`
+
+function App() {
+  const values: RGB[] = []
+  for (let red = 7; red <= 255; red += 8) {
+    for (let green = 7; green <= 255; green += 8) {
+      for (let blue = 7; blue <= 255; blue += 8) {
+        values.push({red, green, blue})
+      }
+    }
+  }
+
+  return (
+    <>
+      {values.map(({red, green, blue}, i) => {
+        return (
+          <ColourBox red={red} green={green} blue={blue} key={i}>
+          </ColourBox>
+        )
+      })}
+    </>
+  )
+}
+
+export default App
